349a60ea3eb85195037f963fbc0313aa9aee3dbd
Antrac1t <33174552+Antrac1t@users.noreply.github.com>
2026-02-22 15:27:02 +0100

    Update __init__.py

--- custom_components/heatzone/__init__.py
+++ custom_components/heatzone/__init__.py
@@ -1,20 +1,21 @@
 """The Thermozona Gas integration."""
+from __future__ import annotations
+
 import logging
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
 from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
 from .coordinator import ThermoZonaCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 
-PLATFORMS = [Platform.CLIMATE]
+PLATFORMS: list[Platform] = [Platform.CLIMATE]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Thermozona Gas from a config entry."""
-    
     coordinator = ThermoZonaCoordinator(hass, entry)
     await coordinator.async_config_entry_first_refresh()
     
@@ -28,9 +29,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-    
-    if unload_ok:
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
         hass.data[DOMAIN].pop(entry.entry_id)
     
     return unload_ok
